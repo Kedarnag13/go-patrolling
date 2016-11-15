bdb728335495d9c786d0efe21a9cea3b72c1aa7f
Kedarnag Mukanahallipatna <kedarnag138@gmail.com>
2016-11-15 20:13:52 +0530

    [development migrate_to_firebase] Fixes the issue with Registration.

--- api/v1/controllers/account/register.go
+++ api/v1/controllers/account/register.go
@@ -54,11 +54,13 @@ func (r registrationController) Create(rw http.ResponseWriter, req *http.Request
 	if err := f.Child("Users").OrderBy("id").EqualTo(user.Id).Value(&get_users); err != nil {
 		panic(err)
 	}
-	log.Println(get_users)
 
-	for _, value := range get_users {
-		get_user := value.(map[string]interface{})
-		if get_user["mobile_number"] != nil {
+	for key := range get_users {
+		var get_user map[string]interface{}
+		if err := f.Child("Users").Child(key).OrderBy("mobile_number").Value(&get_user); err != nil {
+			panic(err)
+		}
+		if get_user["mobile_number"] == user.MobileNumber {
 			b, err := json.Marshal(models.Message{
 				Success: false,
 				Message: "",
@@ -101,54 +103,65 @@ func (r registrationController) Create(rw http.ResponseWriter, req *http.Request
 			log.Fatal(err)
 		}
 
-		if get_session != nil {
-			b, err := json.Marshal(models.Message{
-				Success: true,
-				Message: "Session already exists!",
-				Error:   "",
-			})
-			if err != nil {
-				panic(err)
-			}
-			rw.Header().Set("Content-Type", "application/json")
-			rw.Write(b)
-			goto end
-		} else {
-			session = models.Session{Id: session_id, MobileNumber: user.MobileNumber, UserID: user_id, DeviseToken: user.DeviseToken}
-			child_session, err := f.Child("Sessions").Push(session)
-			if err != nil || child_session == nil {
-				panic(err)
-			}
-			generate_device_id := fireauth.New("go-patrolling")
+		var get_sessions map[string]interface{}
+		if err := f.Child("Sessions").OrderBy("id").EqualTo(session.Id).Value(&get_sessions); err != nil {
+			panic(err)
+		}
 
-			data := fireauth.Data{"uid": "1"}
-			options := &fireauth.Option{
-				NotBefore:  2,
-				Expiration: 3,
-				Admin:      false,
-				Debug:      true,
-			}
-			device_id, err := generate_device_id.CreateToken(data, options)
-			if err != nil {
-				log.Fatal(err)
-			}
-			device = models.Device{Id: device_id, Token: user_id}
-			child_device, err := f.Child("Devices").Push(device)
-			if err != nil || child_device == nil {
+		for key := range get_sessions {
+			var get_session map[string]interface{}
+			if err := f.Child("Sessions").Child(key).EqualTo(user.MobileNumber).OrderBy("mobile_number").Value(&get_session); err != nil {
 				panic(err)
 			}
-			b, err := json.Marshal(models.Message{
-				Success: true,
-				Message: "Session created Successfully!",
-				Error:   "",
-			})
-			if err != nil {
-				panic(err)
+			if get_session["mobile_number"] != nil {
+				b, err := json.Marshal(models.Message{
+					Success: true,
+					Message: "Session already exists!",
+					Error:   "",
+				})
+				if err != nil {
+					panic(err)
+				}
+				rw.Header().Set("Content-Type", "application/json")
+				rw.Write(b)
+				goto end
+			} else {
+				session = models.Session{Id: session_id, MobileNumber: user.MobileNumber, UserID: user_id, DeviseToken: user.DeviseToken}
+				child_session, err := f.Child("Sessions").Push(session)
+				if err != nil || child_session == nil {
+					panic(err)
+				}
+				generate_device_id := fireauth.New("go-patrolling")
+
+				data := fireauth.Data{"uid": "1"}
+				options := &fireauth.Option{
+					NotBefore:  2,
+					Expiration: 3,
+					Admin:      false,
+					Debug:      true,
+				}
+				device_id, err := generate_device_id.CreateToken(data, options)
+				if err != nil {
+					log.Fatal(err)
+				}
+				device = models.Device{Id: device_id, Token: user_id}
+				child_device, err := f.Child("Devices").Push(device)
+				if err != nil || child_device == nil {
+					panic(err)
+				}
+				b, err := json.Marshal(models.Message{
+					Success: true,
+					Message: "Session created Successfully!",
+					Error:   "",
+				})
+				if err != nil {
+					panic(err)
+				}
+				rw.Header().Set("Content-Type", "application/json")
+				rw.Write(b)
+				flag = 0
+				goto end
 			}
-			rw.Header().Set("Content-Type", "application/json")
-			rw.Write(b)
-			flag = 0
-			goto end
 		}
 	}
 end:
